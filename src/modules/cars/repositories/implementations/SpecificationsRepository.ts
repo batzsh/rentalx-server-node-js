5a416eafafbf07656004040cf1304aa610218168
Henrique Balmant <henrique.balmant@gmail.com>
2022-03-06 23:02:40 -0300

    refact: change model to models

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../models/Specification";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificationsRepository";
 
 export class SpecificationsRepository implements ISpecificationsRepository {
