b79e3e1de1005736b7c215be95f7f6115ffdcd94
Henrique Balmant <henrique.balmant@gmail.com>
2022-04-20 23:45:32 -0300

    refact: add typeorm entities and repositories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -4,8 +4,8 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 export class ListCategoriesController {
   constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
 
-  handle(request: Request, response: Response) {
-    const all = this.listCategoriesUseCase.execute();
+  async handle(request: Request, response: Response) {
+    const all = await this.listCategoriesUseCase.execute();
 
     return response.status(200).json(all);
   }
