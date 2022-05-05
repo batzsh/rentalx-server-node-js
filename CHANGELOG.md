118fbf1d8317486e181b6d30bb2e32bdae7d2366
Henrique Balmant <henrique.balmant@gmail.com>
2022-05-05 14:01:26 -0300

    feat: add user

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,35 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.13] - 2022-05-05
+
+### Added
+
+- Users password cryptography
+
+### Changed
+
+### Removed
+
+### Fixed
+
+## [0.0.12] - 2022-05-05
+
+### Added
+
+- Users table migration
+- User entity
+- User repository
+- Create user use case
+- Create user controller
+- Create user route
+
+### Changed
+
+### Removed
+
+### Fixed
+
 ## [0.0.11] - 2022-04-21
 
 ### Added
---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("This email is already in use");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.userRepository.create({
+      name,
+      email,
+      driver_license,
+      password: passwordHash
+    });
+  }
+}
