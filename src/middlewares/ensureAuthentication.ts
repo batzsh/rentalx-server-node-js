cf69aa9b137721ccefb093cacb1405f81a48c9ee
Henrique Balmant <henrique.balmant@gmail.com>
2022-05-09 13:29:50 -0300

    feat: add users authentication

---
+++ src/middlewares/ensureAuthentication.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthentication(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(token, "a7b71b7b48466113992530a9900c2462") as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    next();
+  } catch {
+    throw new Error("Invalid token");
+  }
+}
